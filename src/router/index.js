9809ac44c10b915823ced0e639c1b6c214cc76c5
chl <382492655@qq.com>
2019-06-20 18:50:55 +0800

    commit message

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import childPage from  '@/components/childPage'
+import index from '@/compomemts/index'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/',
       name: 'childPage',
       component: childPage
+    },
+    {
+      path: '/',
+      name: 'index',
+      component: index
     }
   ]
 })
